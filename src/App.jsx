e677276e76fe6876bd2421dc58c0fced808ae4ff
Shotzypoo <sheffieldjoshua18@gmail.com>
2026-07-25 22:04:51 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ const BRAND = {
   slogan: "If you ain’t sneasin, it ain’t seasoned.",
 };
 
-const DROP_001_IMAGE = "/images/2113a.png";
-const DROP_002_IMAGE = "/images/drop-002.jpg";
+const DROP_001_IMAGE = "/images/21280.jpg";
+const DROP_002_IMAGE = "/images/imagesdrop-002.jpg";
 
 const drops = [
   {
